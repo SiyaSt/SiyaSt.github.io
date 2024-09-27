34259be3ef1564d95050e8421a863b2838db9aa0
oooh_shit <anastasiya.stecenko@gmail.com>
2024-09-27 20:10:06 +0300

    some changes

---
+++ web/js/index.js
@@ -0,0 +1,52 @@
+console.log("Hello");
+
+(function () {
+    const startTime = performance.now();
+
+    window.addEventListener('load', function () {
+        const endTime = performance.now();
+        const loadTime = endTime - startTime;
+
+        document.getElementById('load-time').textContent = loadTime.toFixed(2);
+    });
+})();
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('#menu')) {
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const tabs = document.querySelectorAll('.tab');
+    const currentLocation = document.location.pathname;
+    let currentHash = document.location.hash;
+
+    tabs.forEach(tab => {
+        const tabName = tab.getAttribute('data-tab');
+        const tabUrls = {
+            'tab1': '/SiyaSt.github.io/web/index.html',
+            'tab2': '/SiyaSt.github.io/web/aboutTarot.html',
+            'tab3': '/SiyaSt.github.io/web/aboutUs.html'
+        };
+
+        if (currentLocation + currentHash === tabUrls[tabName]) {
+            tab.classList.add('active');
+        } else {
+            tab.classList.remove('active')
+        }
+    });
+
+});
